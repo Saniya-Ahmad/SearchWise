8297fa5760472c81cc52e0d0e0a0264a46d630d3
Saniya Ahmad <saniyanitpatna@gmail.com>
2025-03-16 02:05:50 +0530

    created search page for web and image

---
+++ src/app/search/layout.jsx
@@ -0,0 +1,13 @@
+import SearchHeader from "../components/SearchHeader";
+import './../globals.css'
+
+
+export default function layout({children}) {
+  return (
+    <div>
+        <SearchHeader/>
+    
+      {children}
+    </div>
+  )
+}
