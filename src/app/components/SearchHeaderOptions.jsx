0f81b7809a41dfcf3f77310556bae2a3166fcd97
Saniya Ahmad <saniyanitpatna@gmail.com>
2025-03-18 15:32:54 +0530

    passing params error resolved in images

--- src/app/components/SearchHeaderOptions.jsx
+++ src/app/components/SearchHeaderOptions.jsx
@@ -8,7 +8,7 @@ export default function SearchHeaderOptions() {
     const searchTerm= searchParams.get('searchTerm');
     const pathname = usePathname();
     const selectTab =(tab)=>{
-        router.push(`/search/${tab=='Images'? 'image': 'web'}?searchTerms=${searchTerm}`);
+        router.push(`/search/${tab==='Images'? 'image': 'web'}?searchTerm=${searchTerm}`);
     }
   return (
     <div className='flex space-x-2 select-none border-b w-full justify =-center lg:justify-start
@@ -20,7 +20,7 @@ export default function SearchHeaderOptions() {
         <p>All</p>
         
       </div>
-      <div onClick={()=>selectTab('Images')} className={`flex items-center space-x-1 border-b-4
+      <div onClick={()=>selectTab("Images")} className={`flex items-center space-x-1 border-b-4
          border-transparent active:text-blue-500 cursor-pointer pb-3 px-2
           ${pathname === '/search/image' && '!text-blue-600 !border-blue-600'}`}>
         <AiOutlineCamera className='text-md' />
