e4017e26b9173d046d5b22b70cbc28e8dad3bf15
Saniya Ahmad <saniyanitpatna@gmail.com>
2025-03-18 15:47:23 +0530

    suspense added

--- src/app/search/layout.jsx
+++ src/app/search/layout.jsx
@@ -1,13 +1,17 @@
 import SearchHeader from "../components/SearchHeader";
 import './../globals.css'
-
+import { Suspense } from "react";
 
 export default function layout({children}) {
   return (
     <div>
+      <Suspense>
+        <div>
         <SearchHeader/>
-    
-      {children}
+          {children}
+        </div>
+      </Suspense>
+        
     </div>
   )
 }
